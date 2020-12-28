4359e50cad10eabe8796862e384d75829acdfc07
Liridon Morina <liridon@cyberktechschool.com>
2020-12-28 16:21:04 +0100

    Add tables for e-commerce application

---
+++ src/main/java/com/cybertek/realecommerce/RealECommerceApplication.java
@@ -0,0 +1,13 @@
+package com.cybertek.realecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealECommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealECommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cybertek/realecommerce/RealECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cybertek.realecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
